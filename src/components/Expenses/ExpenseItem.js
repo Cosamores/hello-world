b27e959404cdafbb8a5536d479e2037e8043fadb
Diego Soares <diegocosamores@gmail.com>
2022-12-25 19:02:29 -0300

    bought the course

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,16 +7,15 @@ const ExpenseItem = (props) => {
 
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
+  const clickHandler = (event) => {
+    setTitle('event.target.value');
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">R${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
